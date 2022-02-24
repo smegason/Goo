4dc33d713ce96275a85b85153889ba575f54600d
mfmitsch <mfmitsch@iu.edu>
2022-02-24 08:45:40 -0500

    can copy script to clipboard

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from tkinter import ttk
 from add_cell_controller import add_cell_controller
 
 root = Tk()
+#root.withdraw()
+
+# have all the cells call divide and check their size 
 
 addCellController = add_cell_controller()
 
@@ -133,7 +136,8 @@ adhesion_sliders = []
 #Place widgets for "Cell Adhesion" Section
 
 #Create widgets for "Render" Section
-checkbox = Checkbutton(render_section,text="Rendering")
+checkbox_var = IntVar()
+checkbox = Checkbutton(render_section,text="Rendering",variable=checkbox_var)
 #Label(render_section,text="Rendering").grid(column=1,row=0,padx=(5,776),columnspan=5)
 FilePathLabel = Label(render_section,text="FilePath:")
 FilePathInput = Entry(render_section,width=30)
@@ -158,12 +162,55 @@ gen_script_button.grid(row=1,column=1, padx=65)
 save_script_button.grid(row=1,column=2,padx=65)
 run_script_button.grid(row=1,column=3,padx=65)
 
+class Script:
+    def __init__(self):
+        self.script = ""
+        self.cells = []
+        self.imports = ["from Goo import Goo","import importlib","importlib.reload(Goo)","import bpy"]
+        return
+    def generateScript(self):
+        self.script = ""
+        for line in self.imports:
+            self.script += line + "\n"
+        self.script += "Goo.setup_world()\n" #setup
+        self.script += "collection = bpy.context.blend_data.collections.new(name='Cells')\n" #add collection to store cells
+        self.script += "bpy.context.collection.children.link(collection)\n"
+        self.script += "layer_collection = bpy.context.view_layer.layer_collection.children[collection.name]\n" 
+        self.script += "bpy.context.view_layer.active_layer_collection = layer_collection\n" # make the cells collection active
+        self.script += "bpy.app.handlers.frame_change_post.clear()\n" #clear handlers before adding new ones
+        for cell in self.cells:
+            location = "("+str(cell.location[0])+","+str(cell.location[1])+","+str(cell.location[2])+")"
+            self.script += 'cell = Goo.Cell(name_string = "cell_'+location+'", loc = '+location+')\n'
+            self.script += "Goo.make_cell(cell)\n"
+            #self.script += 'obj = bpy.data.objects["cell_"'+location+']]\n'
+        if division_sliders[0].get():
+            self.script += "bpy.app.handlers.frame_change_post.append(Goo.div_handler)\n"
+        if checkbox_var.get() == 1:
+            self.script += "scene = bpy.context.scene\n"
+            self.script += "fp = '/Users/michaelmitschjr/Desktop/Python/data/'\n"
+            self.script += "scene.render.image_settings.file_format = 'PNG'\n"
+            self.script += "Goo.render(fp,scene,1,60)\n"
+        print(self.script)
+        return
+
+    def saveScript(self):
+        root.clipboard_clear()
+        root.clipboard_append(self.script)
+        root.update() # now it stays on the clipboard after the window is closed
+        return
+    def runScript(self):
+
+        return
+
+script = Script()
+
 def addCell(event):
     location = (x_input.get(),y_input.get(),z_input.get())
     size = cell_size_slider.get()
     cellType = addCellController.cell_types[cell_type_dropdown.current()]
     cell = addCellController.add_cell(size,location,cellType)
     cells_list.insert(len(addCellController.active_cells),cell.name)
+    script.cells += [cell]
     if cell.type in addCellController.active_types:
         return
     addCellController.active_types.append(cell.type)
@@ -188,6 +235,7 @@ def removeCell(event):
     index = cells_list.curselection()[0]
     t = addCellController.active_cells[index].type
     cells_list.delete(index)
+    script.cells.pop(index)
     isTypeGone,row = addCellController.remove_cell(index)
     if isTypeGone:
         division_labels[row].destroy()
@@ -207,13 +255,13 @@ def removeCell(event):
     return
 
 def generateScript(event):
-
+    script.generateScript()
     return
 def saveScript(event):
-
+    script.saveScript()
     return
 def runScript(event):
-
+    script.runScript()
     return
     
 
@@ -222,4 +270,36 @@ cell_type_dropdown['values'] = addCellController.cell_types
 remove_cell_button.bind("<Button-1>",removeCell)
 add_cell_button.bind("<Button-1>",addCell)
 
+gen_script_button.bind("<Button-1>",generateScript)
+save_script_button.bind("<Button-1>",saveScript)
+
 root.mainloop()
+
+'''
+from Goo import Goo
+import importlib
+importlib.reload(Goo)
+import bpy
+Goo.setup_world()
+collection = bpy.context.blend_data.collections.new(name='Cells')
+bpy.context.collection.children.link(collection)
+
+bpy.app.handlers.frame_change_post.clear()
+cell = Goo.Cell(name_string = "cell_(0,0,0)", loc = (0,0,0))
+Goo.make_cell(cell)
+obj = bpy.data.objects["cell_(0,0,0)"]
+bpy.ops.collection.objects_remove_all()
+bpy.data.collections['Cells'].objects.link(obj)
+
+cell = Goo.Cell(name_string = "cell_(0,0,1)", loc = (0,0,2))
+Goo.make_cell(cell)
+obj = bpy.data.objects["cell_(0,0,1)"]
+bpy.ops.collection.objects_remove_all()
+bpy.data.collections['Cells'].objects.link(obj)
+
+scene = bpy.context.scene
+old_fp = '/tmp/'
+fp = '/Users/michaelmitschjr/Desktop/Python/data/'
+scene.render.image_settings.file_format = 'PNG'
+Goo.render(fp,scene,1,60)
+'''
