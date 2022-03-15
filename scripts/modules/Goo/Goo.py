6f09dfc81c316443c041a8dee8757ddc42032549
Sean Megason <megason@hms.harvard.edu>
2022-03-15 15:55:39 -0400

    Cleaned up add_materials code

--- scripts/modules/Goo/Goo.py
+++ scripts/modules/Goo/Goo.py
@@ -405,141 +405,6 @@ class Cell():
         daughter2.data['daughters'] = ['none', 'none']
         return daughter1, daughter2
 
-def add_material(mat):
-    bpy.data.materials.new(name = mat.name)
-    bpy.data.materials[mat.name].node_tree.nodes["Mix Shader"].inputs[0].default_value = mat.BDSF_1_fac
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[0].default_value = mat.BDSF_1_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[1].default_value = mat.BDSF_1_subsurface
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[0] = mat.BDSF_1_subsurf_radius[0]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[1] = mat.BDSF_1_subsurf_radius[1]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[2] = mat.BDSF_1_subsurf_radius[2]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[3].default_value = mat.BDSF_1_subsurf_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[4].default_value = mat.BDSF_1_metallic
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[5].default_value = mat.BDSF_1_specular
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[6].default_value = mat.BDSF_1_specular_tint
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[7].default_value = mat.BDSF_1_roughness
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[8].default_value = mat.BDSF_1_anisotropic
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[9].default_value = mat.BDSF_1_anisotropic_rot
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[10].default_value = mat.BDSF_1_sheen
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[11].default_value = mat.BDSF_1_sheen_tint
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[12].default_value = mat.BDSF_1_clearcoat
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[13].default_value = mat.BDSF_1_clear_rough
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[14].default_value = mat.BDSF_1_IOR
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[15].default_value = mat.BDSF_1_transmission
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[16].default_value = mat.BDSF_1_transmission_rough
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[17].default_value = mat.BDSF_1_emission_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[18].default_value = mat.BDSF_1_emission_strength
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[19].default_value = mat.BDSF_1_alpha
-    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[0].default_value = mat.BDSF_2_hue
-    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[1].default_value = mat.BDSF_2_saturation
-    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[2].default_value = mat.BDSF_2_value
-    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[3].default_value = mat.BDSF_2_fac
-    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[4].default_value = mat.BDSF_2_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[1].default_value = mat.BDSF_2_subsurface
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[0] = mat.BDSF_2_subsurf_radius[0]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[1] = mat.BDSF_2_subsurf_radius[1]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[2] = mat.BDSF_2_subsurf_radius[2]
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = mat.BDSF_2_subsurf_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[4].default_value = mat.BDSF_2_metallic
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[5].default_value = mat.BDSF_1_specular
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[6].default_value = mat.BDSF_2_specular_tint
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[7].default_value = mat.BDSF_2_roughness
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[8].default_value = mat.BDSF_2_anisotropic
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[9].default_value = mat.BDSF_2_anisotropic_rot
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[10].default_value = mat.BDSF_2_sheen
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[11].default_value = mat.BDSF_2_sheen_tint
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[12].default_value = mat.BDSF_2_clearcoat
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[13].default_value = mat.BDSF_2_clear_rough
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[14].default_value = mat.BDSF_2_IOR
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[15].default_value = mat.BDSF_2_transmission
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[16].default_value = mat.BDSF_2_transmission_rough
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[17].default_value = mat.BDSF_2_emission_color
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[18].default_value = mat.BDSF_2_emission_strength
-    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[19].default_value = mat.BDSF_2_alpha
-    bpy.context.object.active_material.blend_method = mat.blend_method
-    bpy.context.object.active_material.shadow_method = mat.shadow_method
-    bpy.context.object.active_material.refraction_depth = mat.refraction_depth
-    bpy.context.object.active_material.pass_index = mat.pass_index
-    bpy.context.object.active_material.diffuse_color = mat.diffuse_color
-    bpy.context.object.active_material.metallic = mat.metallic
-    bpy.context.object.active_material.roughness = mat.roughness
-    bpy.ops.node.add_node(type="ShaderNodeTexNoise", use_transform=True)
-    bpy.ops.node.select(wait_to_deselect_others=True, mouse_x=114, mouse_y=765, extend=False, deselect_all=True)
-    bpy.ops.node.link(detach=False)
-
-def add_material2(mat_name):
-    mat = bpy.data.materials.get(mat_name)
-    if mat is None:
-        print ("Material not found. Make new")
-        mat = bpy.data.materials.new(name=mat_name)
-     
-    if mat.node_tree:
-        mat.node_tree.links.clear()
-        mat.node_tree.nodes.clear()
-            
-    bpy.data.materials[mat.name].use_nodes = True
-    
-    #soap bubble shader
-#    bpy.data.materials[mat.name].node_tree.nodes["Mix Shader"].inputs[0].default_value = mat.BDSF_1_fac
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[0].default_value = mat.BDSF_1_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[1].default_value = mat.BDSF_1_subsurface
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[0] = mat.BDSF_1_subsurf_radius[0]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[1] = mat.BDSF_1_subsurf_radius[1]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[2].default_value[2] = mat.BDSF_1_subsurf_radius[2]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[3].default_value = mat.BDSF_1_subsurf_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[4].default_value = mat.BDSF_1_metallic
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[5].default_value = mat.BDSF_1_specular
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[6].default_value = mat.BDSF_1_specular_tint
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[7].default_value = mat.BDSF_1_roughness
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[8].default_value = mat.BDSF_1_anisotropic
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[9].default_value = mat.BDSF_1_anisotropic_rot
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[10].default_value = mat.BDSF_1_sheen
-#   bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[11].default_value = mat.BDSF_1_sheen_tint
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[12].default_value = mat.BDSF_1_clearcoat
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[13].default_value = mat.BDSF_1_clear_rough
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[14].default_value = mat.BDSF_1_IOR
-#   bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[15].default_value = mat.BDSF_1_transmission
-#   bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[16].default_value = mat.BDSF_1_transmission_rough
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[17].default_value = mat.BDSF_1_emission_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[18].default_value = mat.BDSF_1_emission_strength
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF"].inputs[19].default_value = mat.BDSF_1_alpha
-#   bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[0].default_value = mat.BDSF_2_hue
-#    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[1].default_value = mat.BDSF_2_saturation
-#   bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[2].default_value = mat.BDSF_2_value
-#    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[3].default_value = mat.BDSF_2_fac
-#    bpy.data.materials[mat.name].node_tree.nodes["Hue Saturation Value"].inputs[4].default_value = mat.BDSF_2_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[1].default_value = mat.BDSF_2_subsurface
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[0] = mat.BDSF_2_subsurf_radius[0]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[1] = mat.BDSF_2_subsurf_radius[1]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[2].default_value[2] = mat.BDSF_2_subsurf_radius[2]
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = mat.BDSF_2_subsurf_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[4].default_value = mat.BDSF_2_metallic
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[5].default_value = mat.BDSF_1_specular
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[6].default_value = mat.BDSF_2_specular_tint
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[7].default_value = mat.BDSF_2_roughness
- #   bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[8].default_value = mat.BDSF_2_anisotropic
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[9].default_value = mat.BDSF_2_anisotropic_rot
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[10].default_value = mat.BDSF_2_sheen
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[11].default_value = mat.BDSF_2_sheen_tint
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[12].default_value = mat.BDSF_2_clearcoat
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[13].default_value = mat.BDSF_2_clear_rough
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[14].default_value = mat.BDSF_2_IOR
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[15].default_value = mat.BDSF_2_transmission
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[16].default_value = mat.BDSF_2_transmission_rough
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[17].default_value = mat.BDSF_2_emission_color
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[18].default_value = mat.BDSF_2_emission_strength
-#    bpy.data.materials[mat.name].node_tree.nodes["Principled BSDF.001"].inputs[19].default_value = mat.BDSF_2_alpha
-#    bpy.context.object.active_material.blend_method = mat.blend_method
-#    bpy.context.object.active_material.shadow_method = mat.shadow_method
-#    bpy.context.object.active_material.refraction_depth = mat.refraction_depth
-##    bpy.context.object.active_material.pass_index = mat.pass_index
-#    bpy.context.object.active_material.diffuse_color = mat.diffuse_color
-#    bpy.context.object.active_material.metallic = mat.metallic
-#    bpy.context.object.active_material.roughness = mat.roughness
-#    bpy.ops.node.add_node(type="ShaderNodeTexNoise", use_transform=True)
-#    bpy.ops.node.select(wait_to_deselect_others=True, mouse_x=114, mouse_y=765, extend=False, deselect_all=True)
-#    bpy.ops.node.link(detach=False)
-
 def add_material_cell(mat_name, r, g, b):
     #adds a soap bubble like material for use in rendering cells
     print ("add cell material " + mat_name)
@@ -630,65 +495,6 @@ def add_material_cell(mat_name, r, g, b):
     link_main_mix = links.new(node_main.outputs[0], node_mix.inputs[1])
     link_random_mix = links.new(node_random.outputs[0], node_mix.inputs[2])
     link_mix_out = links.new(node_mix.outputs[0], node_output.inputs[0])
-        
-
-class Material():
-    def __init__(self):
-        self.name = "Cell Material"
-        self.BDSF_1_fac = 0.079
-        self.BDSF_1_color = (0.00749999, 0.020955, 0.3, 1)
-        self.BDSF_1_subsurface = 0
-        self.BDSF_1_subsurf_radius = (1.1, 0.2, 0.2)
-        self.BDSF_1_subsurf_color = (0.8, 0.8, 0.8, 1)
-        self.BDSF_1_metallic = 0.136
-        self.BDSF_1_specular = 0.5
-        self.BDSF_1_specular_tint = 0.555
-        self.BDSF_1_roughness = 0.318
-        self.BDSF_1_anisotropic = 0.041
-        self.BDSF_1_anisotropic_rot = 0.048
-        self.BDSF_1_sheen = 0.052
-        self.BDSF_1_sheen_tint = 0.03
-        self.BDSF_1_clearcoat = 0.114
-        self.BDSF_1_clear_rough = 0.123
-        self.BDSF_1_IOR = 1.45
-        self.BDSF_1_transmission = 0.882
-        self.BDSF_1_transmission_rough = 0
-        self.BDSF_1_emission_color = (0, 0, 0, 1)
-        self.BDSF_1_emission_strength = 1
-        self.BDSF_1_alpha = 0.414
-
-        self.BDSF_2_hue = 0.8
-        self.BDSF_2_saturation = 2
-        self.BDSF_2_value = 2
-        self.BDSF_2_fac = 1
-        self.BDSF_2_color = (0.8, 0.8, 0.8, 1)
-        self.BDSF_2_subsurface = 0
-        self.BDSF_2_subsurf_radius = (1.1, 0.2, 0.2)
-        self.BDSF_2_subsurf_color = (0.8, 0.8, 0.8, 1)
-        self.BDSF_2_metallic = 0
-        self.BDSF_2_specular = 0.5
-        self.BDSF_2_specular_tint = 0
-        self.BDSF_2_roughness = 0.482
-        self.BDSF_2_anisotropic = 0
-        self.BDSF_2_anisotropic_rot = 0
-        self.BDSF_2_sheen = 0
-        self.BDSF_2_sheen_tint = 0
-        self.BDSF_2_clearcoat = 0
-        self.BDSF_2_clear_rough = 0
-        self.BDSF_2_IOR = 1.45
-        self.BDSF_2_transmission = 1
-        self.BDSF_2_transmission_rough = 0
-        self.BDSF_2_emission_color = (0, 0, 0, 1)
-        self.BDSF_2_emission_strength = 1
-        self.BDSF_2_alpha = 0.555
-
-        self.blend_method = 'BLEND'
-        self.shadow_method = 'NONE'
-        self.refraction_depth = 0
-        self.pass_index = 0
-        self.diffuse_color = (0.8, 0.8, 0.8, 1)
-        self.metallic = 0
-        self.roughness = 0.4
 
 def initialize_cells(num_cells, loc_array, material):
     if len(num_cells) != len(loc_array):
@@ -700,18 +506,16 @@ def initialize_cells(num_cells, loc_array, material):
         make_cell(cell)
 
 def setup_world():
-    print("setup worldDDDD")
+    print("setup world")
     bpy.context.scene.use_gravity = False
     bpy.context.scene.unit_settings.system = 'METRIC'
     bpy.context.scene.unit_settings.scale_length = 1
     bpy.context.scene.unit_settings.system_rotation = 'DEGREES'
-    bpy.context.scene.unit_settings.length_unit = 'METERS'
-    bpy.context.scene.unit_settings.mass_unit = 'KILOGRAMS'
+    bpy.context.scene.unit_settings.length_unit = 'MICROMETERS'
+    bpy.context.scene.unit_settings.mass_unit = 'MILLIGRAMS'
     bpy.context.scene.unit_settings.time_unit = 'SECONDS'
-    bpy.context.scene.unit_settings.temperature_unit = 'KELVIN'
+    bpy.context.scene.unit_settings.temperature_unit = 'CELSIUS'
 
-    #add materials
-    
     add_world_HDRI()
   
 def add_world_HDRI():
@@ -732,7 +536,8 @@ def add_world_HDRI():
     # Add Environment Texture node
     node_environment = tree_nodes.new('ShaderNodeTexEnvironment')
     # Load and assign the image to the node property
-    node_environment.image = bpy.data.images.load("//missile_launch_facility_01_4k.hdr") # Relative path- must be in same directory as blend file
+    scripts_paths = bpy.utils.script_paths()
+    node_environment.image = bpy.data.images.load(scripts_paths[-1]+"/modules/Goo/missile_launch_facility_01_4k.hdr") # Relative path- this file must be in same directory as blend file
     node_environment.location = -300,0
 
     # Add Output node
