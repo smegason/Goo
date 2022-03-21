615cf4a8c939b5a654cf22332ae06e0c639bfe60
Sean Megason <megason@hms.harvard.edu>
2022-03-21 14:37:57 -0400

    x

--- scripts/modules/Goo/Goo.py
+++ scripts/modules/Goo/Goo.py
@@ -1,7 +1,7 @@
 # Goo.py - This is the Goo library. It contains all helper functions for Goo
 # Goo is licensed under BSDv2
 
-import bpy, bmesh, mathutils, numpy as np
+#import bpy, bmesh, mathutils, numpy as np
 
 def calculate_volume(obj):
     """
