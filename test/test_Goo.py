0743708427a4bf3809e39c10a7115411333553af
Sean Megason <megason@hms.harvard.edu>
2022-03-27 13:44:52 -0400

    setting up pytest

---
+++ test/test_Goo.py
@@ -0,0 +1,2 @@
+#test_goo.py
+#this file is read by pytest to test all functions in Goo
---
+++ tests/test_Goo.py
@@ -0,0 +1,13 @@
+#test_Goo.py
+#this file is read by pytest to test all functions in Goo
+
+from Goo import Goo
+
+def test_calculate_volume():
+    #obj = ???? need to get Blender object to test with somehow
+    #volume = Goo.calculate_volume(obj)
+    #assert volume = ??
+
+    assert True
+
+    
