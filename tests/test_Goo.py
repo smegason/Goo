9425a811b925b7aa200d5688461f66c00342e6ae
Sean Megason <megason@hms.harvard.edu>
2022-03-27 14:36:53 -0400

    test

--- tests/test_Goo.py
+++ tests/test_Goo.py
@@ -2,7 +2,7 @@
 #this file is read by pytest to test all functions in Goo
 
 #from Goo import Goo
-import "/scripts/modules/Goo/Goo.py"
+import '/scripts/modules/Goo/Goo.py'
 
 def test_calculate_volume():
     #obj = ???? need to get Blender object to test with somehow
