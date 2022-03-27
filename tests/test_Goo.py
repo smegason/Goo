bc79a4dc8cbccf70d64bc27369d681ac0264ba22
Sean Megason <megason@hms.harvard.edu>
2022-03-27 15:17:30 -0400

    Update test_Goo.py

--- tests/test_Goo.py
+++ tests/test_Goo.py
@@ -1,6 +1,10 @@
 #test_Goo.py
 #this file is read by pytest to test all functions in Goo
 
+import pprint
+import sys
+pprint.pprint(sys.path)
+
 from Goo import Goo
 #import '/scripts/modules/Goo/Goo.py'
 
