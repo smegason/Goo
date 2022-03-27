5a7c6735b8f06013c89d15ab397f45deeb9487e3
Sean Megason <megason@hms.harvard.edu>
2022-03-27 15:39:14 -0400

    Update test_Goo.py

--- tests/test_Goo.py
+++ tests/test_Goo.py
@@ -3,6 +3,11 @@
 
 #from Goo import Goo
 
+def test_sample_func():
+    ret = Goo.sample_func()
+    print (ret)
+    assert True
+
 def test_calculate_volume():
     #obj = ???? need to get Blender object to test with somehow
     #volume = calculate_volume(obj)
