fe8eea9aa664ccd6e70bafd2bc023e347e03f4a5
Sean Megason <megason@hms.harvard.edu>
2022-03-27 16:16:36 -0400

    test

--- tests/test_Goo.py
+++ tests/test_Goo.py
@@ -1,11 +1,11 @@
 #test_Goo.py
 #this file is read by pytest to test all functions in Goo
 
-import Goo
+import goo
 
 def test_sample_func():
 #    ret = Goo.sample_func()
-    ret = Goo.sample_func()
+    ret = goo.sample_func()
     print (ret)
     assert True
 
