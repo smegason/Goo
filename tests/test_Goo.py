cb99252300fc3131cbf9b376b629b0ab4d18bcc3
Sean Megason <megason@hms.harvard.edu>
2022-03-29 09:51:00 -0400

    Update test_Goo.py

--- tests/test_Goo.py
+++ tests/test_Goo.py
@@ -1,20 +1,12 @@
 #test_goo.py
-#this file is read by pytest to test all functions in Goo cccccccc
+#this file is read by pytest to test all functions in Goo 
 
 #import Goo
-
-class classA():
-    def __init__(self):
-        self.var1 = 0
-        self.var2 = "Hello"
-
-A = classA()
-
+import bpy
 
 def test_sample_func():
     print("test samp[le function")
-    print ("A")
-    print(dir(classA))
+
 
     #print ("Goo class:")
     #print (dir(Goo))
@@ -24,6 +16,8 @@ def test_sample_func():
     #Goo.sample_func()
     #print (ret)
     print("Test sample func")
+    version = bpy.app.version_string
+    print (version)
     assert 1 == 1   
 
 def test_calculate_volume():
