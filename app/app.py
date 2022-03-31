4111c530c6f0f20479c47b11730ff722313ae85b
Sean Megason <megason@hms.harvard.edu>
2022-03-31 18:15:14 -0400

    linting

--- app/app.py
+++ app/app.py
@@ -40,6 +40,7 @@ class Intry(Entry):
         self.var.trace('w', self.check)
         self.get, self.set = self.var.get, self.var.set
 
+
     def check(self, *args):
         #if self.get().isdecimal(): 
         try:
@@ -49,6 +50,7 @@ class Intry(Entry):
         except:
             # there's non-digit characters in the input; reject this 
             self.set(self.old_value)
+
 
 root.title("Goo")
 #root.geometry("500x600+10+20")
@@ -61,74 +63,74 @@ borderThickness = 3
 sectionBackground = "gray"
 
 # Main sections of the app
-add_cell_section = Frame(root,width=main_width,height=section_height,bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
-cell_division_section = Frame(root, width=main_width, height=section_height,bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
-growth_rate_section = Frame(root, width=main_width, height=section_height,bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
-cell_adhesion_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
-render_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
-generate_script_section = Frame(root, width=main_width, height=section_height,bg=sectionBackground,pady=3,highlightbackground=borderColor,highlightthickness=borderThickness)
+add_cell_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
+cell_division_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
+growth_rate_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
+cell_adhesion_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
+render_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
+generate_script_section = Frame(root, width=main_width, height=section_height, bg=sectionBackground,pady=3, highlightbackground=borderColor, highlightthickness=borderThickness)
 
 #Place main sections of the app
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-add_cell_section.pack(side=TOP,pady=3)
-cell_division_section.pack(side=TOP,pady=3)
-growth_rate_section.pack(side=TOP,pady=3)
-cell_adhesion_section.pack(side=TOP,pady=3)
-render_section.pack(side=TOP,pady=3)
-generate_script_section.pack(side=TOP,pady=3)
+add_cell_section.pack(side=TOP, pady=3)
+cell_division_section.pack(side=TOP, pady=3)
+growth_rate_section.pack(side=TOP, pady=3)
+cell_adhesion_section.pack(side=TOP, pady=3)
+render_section.pack(side=TOP, pady=3)
+generate_script_section.pack(side=TOP, pady=3)
 
 #Create widgets for "Initial Cells" Section
 padx = 3
 
-Label(add_cell_section,text="Initial Cells").grid(column=0,row=0,padx=(5,632),columnspan=9)
-cell_type_dropdown = ttk.Combobox(add_cell_section,text="")
+Label(add_cell_section,text="Initial Cells").grid(column=0, row=0, padx=(5, 632), columnspan=9)
+cell_type_dropdown = ttk.Combobox(add_cell_section, text="")
 
-x_lable = Label(add_cell_section,text="x:")
-y_lable = Label(add_cell_section,text="y:")
-z_lable = Label(add_cell_section,text="z:")
-x_input = Lotfi(add_cell_section,width=5)
-y_input = Lotfi(add_cell_section,width=5)
-z_input = Lotfi(add_cell_section,width=5)
-size_lable = Label(add_cell_section,text="Size:")
-cell_size_slider = Scale(add_cell_section,orient=HORIZONTAL,from_=0,to=100)
-cell_size_unit = Label(add_cell_section,text="Unit")
-add_cell_button = Button(add_cell_section,text="Add Cell")
-cells_list = Listbox(add_cell_section,height=4)
+x_lable = Label(add_cell_section, text="x:")
+y_lable = Label(add_cell_section, text="y:")
+z_lable = Label(add_cell_section, text="z:")
+x_input = Lotfi(add_cell_section, width=5)
+y_input = Lotfi(add_cell_section, width=5)
+z_input = Lotfi(add_cell_section, width=5)
+size_lable = Label(add_cell_section, text="Size:")
+cell_size_slider = Scale(add_cell_section, orient=HORIZONTAL,from_=0,to=100)
+cell_size_unit = Label(add_cell_section, text="Unit")
+add_cell_button = Button(add_cell_section, text="Add Cell")
+cells_list = Listbox(add_cell_section, height=4)
 scrollbar = ttk.Scrollbar(
     add_cell_section,
     orient='vertical',
     command=cells_list.yview,
 )
 cells_list['yscrollcommand'] = scrollbar.set
-remove_cell_button = Button(add_cell_section,text="Remove Cell")
+remove_cell_button = Button(add_cell_section, text="Remove Cell")
 
 #PLace widgets for "Initial Cells" Section
-cell_type_dropdown.grid(column=1,row=1,padx=padx)
-x_lable.grid(row=1,column=2,padx=(padx,0))
-x_input.grid(row=1,column=3,padx=(0,padx))
-y_lable.grid(row=1,column=4,padx=(padx,0))
-y_input.grid(row=1,column=5,padx=(0,padx))
-z_lable.grid(row=1,column=6,padx=(padx,0))
-z_input.grid(row=1,column=7,padx=(0,padx))
-size_lable.grid(row=1,column=8,padx=(padx,0))
-cell_size_slider.grid(row=1,column=9)
-cell_size_unit.grid(row=1,column=10,padx=(0,padx))
-add_cell_button.grid(row=2,column=1)
-cells_list.grid(row=2,column=2,columnspan=6)
-scrollbar.grid(row=2,column=6)
-remove_cell_button.grid(row=2,column=8,columnspan=3)
+cell_type_dropdown.grid(column=1, row=1, padx=padx)
+x_lable.grid(row=1, column=2, padx=(padx,0))
+x_input.grid(row=1, column=3, padx=(0,padx))
+y_lable.grid(row=1, column=4, padx=(padx,0))
+y_input.grid(row=1, column=5, padx=(0,padx))
+z_lable.grid(row=1, column=6, padx=(padx,0))
+z_input.grid(row=1, column=7, padx=(0,padx))
+size_lable.grid(row=1, column=8, padx=(padx,0))
+cell_size_slider.grid(row=1, column=9)
+cell_size_unit.grid(row=1, column=10, padx=(0,padx))
+add_cell_button.grid(row=2, column=1)
+cells_list.grid(row=2, column=2, columnspan=6)
+scrollbar.grid(row=2, column=6)
+remove_cell_button.grid(row=2, column=8, columnspan=3)
 
 #Create widgets for "Cell Division" Section
-Label(cell_division_section,text="Cell Division").grid(column=0,row=0,padx=(5,762),columnspan=4)
+Label(cell_division_section,text="Cell Division").grid(column=0, row=0,padx=(5, 762), columnspan=4)
 division_labels = []
 division_sliders = []
 
 #Place widgets for "Cell Division" Section
 
 #Create widgets for "Cell Growth" Section
-Label(growth_rate_section,text="Cell Growth").grid(column=0,row=0,padx=(5,766),columnspan=4)
+Label(growth_rate_section,text="Cell Growth").grid(column=0, row=0, padx=(5, 766), columnspan=4)
 growth_labels = []
 growth_sliders = []
 
@@ -136,7 +138,7 @@ growth_sliders = []
 
 
 #Create widgets for "Cell Adhesion" Section
-Label(cell_adhesion_section,text="Cell Adhesion").grid(column=0,row=0,padx=(5,754),columnspan=4)
+Label(cell_adhesion_section,text="Cell Adhesion").grid(column=0, row=0, padx=(5,754), columnspan=4)
 adhesion_labels = []
 adhesion_sliders = []
 adhesion_dropdowns = []
@@ -146,37 +148,41 @@ adhesion_slider_values = []
 
 #Create widgets for "Render" Section
 checkbox_var = IntVar()
-checkbox = Checkbutton(render_section,text="Rendering",variable=checkbox_var)
+checkbox = Checkbutton(render_section,text="Rendering", variable=checkbox_var)
 #Label(render_section,text="Rendering").grid(column=1,row=0,padx=(5,776),columnspan=5)
-FilePathLabel = Label(render_section,text="FilePath:")
-FilePathInput = Entry(render_section,width=30)
-NumFramesLabel = Label(render_section,text="Number of Frames:")
-NumFrames = Intry(render_section,width=5)
+FilePathLabel = Label(render_section, text="FilePath:")
+FilePathInput = Entry(render_section, width=30)
+NumFramesLabel = Label(render_section, text="Number of Frames:")
+NumFrames = Intry(render_section, width=5)
 
 #Place widgets for "Render" Section
-checkbox.grid(column=0,row=0,padx=(5,758),columnspan=5)
-FilePathLabel.grid(row=1,column=1)
-FilePathInput.grid(row=1,column=2)
-NumFramesLabel.grid(row=1,column=3)
-NumFrames.grid(row=1,column=4)
+checkbox.grid(column=0, row=0,padx=(5, 758), columnspan=5)
+FilePathLabel.grid(row=1, column=1)
+FilePathInput.grid(row=1, column=2)
+NumFramesLabel.grid(row=1, column=3)
+NumFrames.grid(row=1, column=4)
 
 #Create widgets for "Generate Script" Section
-Label(generate_script_section,text="Generation").grid(column=0,row=0,padx=(5,770),columnspan=4)
-gen_script_button = Button(generate_script_section,text="Generate Script")
-save_script_button = Button(generate_script_section,text="Copy Script")
-run_script_button = Button(generate_script_section,text="Run Script")
+Label(generate_script_section,text="Generation").grid(column=0,row=0,padx=(5, 770), columnspan=4)
+gen_script_button = Button(generate_script_section, text="Generate Script")
+save_script_button = Button(generate_script_section, text="Copy Script")
+run_script_button = Button(generate_script_section, text="Run Script")
 
 #Place widgets for "Generate Script" Section
-gen_script_button.grid(row=1,column=1, padx=65)
-save_script_button.grid(row=1,column=2,padx=65)
-run_script_button.grid(row=1,column=3,padx=65)
+gen_script_button.grid(row=1, column=1, padx=65)
+save_script_button.grid(row=1, column=2, padx=65)
+run_script_button.grid(row=1, column=3, padx=65)
 
 class Script:
+
+
     def __init__(self):
         self.script = ""
         self.cells = []
         self.imports = ["from Goo import Goo","import importlib","importlib.reload(Goo)","import bpy"]
         return
+
+
     def generateScript(self):
         self.script = ""
         self.script += "# Import Libraries and setup world\n"
@@ -239,21 +245,28 @@ class Script:
         print(self.script)
         return
 
+
     def saveScript(self):
         root.clipboard_clear()
         root.clipboard_append(self.script)
         root.update() # now it stays on the clipboard after the window is closed
         return
+
+
     def runScript(self):
 
         return
 
 script = Script()
+
+
 def adhesion_dropdown_changed(event):
     dropdown = event.widget
     row = dropdown.grid_info()['row']-1
     key = dropdown.get()
     adhesion_sliders[row].set(adhesion_slider_values[row][key])
+
+
 def adhesion_slider_changed(event):
     slider = event.widget
     row = slider.grid_info()['row']-1
@@ -262,21 +275,23 @@ def adhesion_slider_changed(event):
     adhesion_slider_values[row][key] = int(value)
     print(value)
     print(row)
+
+
 def addCell(event):
     location = (x_input.get(),y_input.get(),z_input.get())
     size = cell_size_slider.get()
     cellType = addCellController.cell_types[cell_type_dropdown.current()]
     cell = addCellController.add_cell(size,location,cellType)
-    cells_list.insert(len(addCellController.active_cells),cell.name)
+    cells_list.insert(len(addCellController.active_cells), cell.name)
     script.cells += [cell]
     if cell.type in addCellController.active_types:
         return
     addCellController.active_types.append(cell.type)
     num_cells = len(addCellController.active_cells)
-    division_labels.append(Label(cell_division_section,text=cell.type))
-    division_sliders.append(Scale(cell_division_section,orient=HORIZONTAL,length=500,from_=0,to=100))
-    division_labels[-1].grid(column=1,row=num_cells,padx=20)
-    division_sliders[-1].grid(row=num_cells,column=2,padx=10)
+    division_labels.append(Label(cell_division_section, text=cell.type))
+    division_sliders.append(Scale(cell_division_section, orient=HORIZONTAL, length=500, from_=0, to=100))
+    division_labels[-1].grid(column=1, row=num_cells, padx=20)
+    division_sliders[-1].grid(row=num_cells, column=2, padx=10)
 
     growth_labels.append(Label(growth_rate_section,text=cell.type))
     growth_sliders.append(Scale(growth_rate_section,orient=HORIZONTAL,length=500,from_=0,to=100))
@@ -284,19 +299,20 @@ def addCell(event):
     growth_sliders[-1].grid(row=num_cells,column=2,padx=10)
 
     adhesion_labels.append(Label(cell_adhesion_section,text=cell.type))
-    adhesion_dropdowns.append(ttk.Combobox(cell_adhesion_section,text="",name=cell.type))
-    adhesion_sliders.append(Scale(cell_adhesion_section,orient=HORIZONTAL,length=300,from_=-800,to=800))
-    adhesion_labels[-1].grid(column=1,row=num_cells,padx=20)
-    adhesion_dropdowns[-1].grid(column=2, row=num_cells,padx=20)
-    adhesion_sliders[-1].grid(row=num_cells,column=3,padx=10)
+    adhesion_dropdowns.append(ttk.Combobox(cell_adhesion_section, text="", name=cell.type))
+    adhesion_sliders.append(Scale(cell_adhesion_section,orient=HORIZONTAL, length=300, from_=-800, to=800))
+    adhesion_labels[-1].grid(column=1, row=num_cells, padx=20)
+    adhesion_dropdowns[-1].grid(column=2, row=num_cells, padx=20)
+    adhesion_sliders[-1].grid(row=num_cells, column=3, padx=10)
     adhesion_dropdowns[-1].bind("<<ComboboxSelected>>", adhesion_dropdown_changed)
-    adhesion_sliders[-1].bind("<ButtonRelease>",adhesion_slider_changed)
+    adhesion_sliders[-1].bind("<ButtonRelease>", adhesion_slider_changed)
     forces = {}
     for t in addCellController.active_types:
         forces[t] = 0
     adhesion_slider_values.append(forces)
     update_adhesion_dropdown()
     return
+
 
 def removeCell(event):
     index = cells_list.curselection()[0]
@@ -324,6 +340,8 @@ def removeCell(event):
         #adhesion_slider_values.remove(adhesion_slider_values[row])
         update_adhesion_dropdown()
     return
+
+
 def update_adhesion_dropdown():
     for dropdown in adhesion_dropdowns:
         dropdown['values'] = addCellController.active_types
@@ -331,12 +349,18 @@ def update_adhesion_dropdown():
         for dict in adhesion_slider_values:
             if not (t in dict):
                 dict[t] = 0
+
+
 def generateScript(event):
     script.generateScript()
     return
+
+
 def saveScript(event):
     script.saveScript()
     return
+
+
 def runScript(event):
     script.runScript()
     return
@@ -344,13 +368,12 @@ def runScript(event):
 
 # add bindings to interface
 cell_type_dropdown['values'] = addCellController.cell_types
-remove_cell_button.bind("<Button-1>",removeCell)
-add_cell_button.bind("<Button-1>",addCell)
+remove_cell_button.bind("<Button-1>", removeCell)
+add_cell_button.bind("<Button-1>", addCell)
 
-gen_script_button.bind("<Button-1>",generateScript)
-save_script_button.bind("<Button-1>",saveScript)
+gen_script_button.bind("<Button-1>", generateScript)
+save_script_button.bind("<Button-1>", saveScript)
 
 root.mainloop()
-
 
 # forces that affect one type of cell should all be in the same collection
