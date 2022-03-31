e525dbc2bb3fc1d09ec6342277aed5a29790d716
Sean Megason <megason@hms.harvard.edu>
2022-03-31 17:03:09 -0400

    cleaning code after test setup

--- tests/test_goo.py
+++ tests/test_goo.py
@@ -1,42 +1,13 @@
 #test_goo.py
 #this file is read by pytest to test all functions in goo
 
-# importing module
-#import sys
-#sys.path.append('scripts/modules')
-
-# importing required module
-#import goo
 import bpy
 
-import os
-print ("Root dir=")
-print (os.listdir())
-
-#os.chdir("..")
-#print("One up dir=")
-#print (os.listdir())
-#print ("-----")
-
-#os.chdir("..")
-#print("Two up dir=")
-#print (os.listdir())
-#print ("-----")
-
-#os.chdir("..")
-#print("Three up dir=")
-#print (os.listdir())
-#print ("-----")
-
-os.chdir("scripts/modules/goo")
-print ("modules dir=")
-print (os.listdir())   # For some weird reason goo.py is not in this directory but other .py files are e.g. goo2.py
-
+# importing goo
 import importlib.util
-spec = importlib.util.spec_from_file_location("goo", "goo.py")
+spec = importlib.util.spec_from_file_location("goo", "scripts/modules/goo/goo.py")
 goo = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(goo)
-#print(foo.var)
 
 def test_sample_func():
     print("test sample function------------")
@@ -55,8 +26,10 @@ def test_sample_func():
 
 
 def test_calculate_volume():
-    #obj = ???? need to get Blender object to test with somehow
-    #volume = calculate_volume(obj)
+    #make cell
+    
+    
+    #volume = goo.calculate_volume(obj)
     #assert volume = ??
     print ("test calculate volume")
 
