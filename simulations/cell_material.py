cd324e41d7f7f4e59f3f2b9b16f6aa19ffbffd73
Sean Megason <megason@hms.harvard.edu>
2022-04-07 23:11:13 -0400

    Update cell_material.py

--- simulations/cell_material.py
+++ simulations/cell_material.py
@@ -1,17 +1,16 @@
 # hello_world.py - creates one cell
 
-import bpy
 from goo import goo
 from importlib import reload
 reload(goo)
 
 goo.setup_world()
 
-goo.add_material_cell("CellGreen",0.007,0.300,0.005)
-goo.add_material_cell("CellBlue",0.007, 0.021, 0.300)
+goo.add_material_cell("CellGreen", 0.007, 0.300, 0.005)
+goo.add_material_cell("CellBlue", 0.007, 0.021, 0.300)
 
-cell = goo.Cell(name_string = "Cell1_", loc = (0, 0, 0), material = "CellGreen")
+cell = goo.Cell(name_string="Cell1_", loc=(0, 0, 0), material="CellGreen")
 goo.make_cell(cell)
 
-cell2 = goo.Cell(name_string = "Cell2_", loc = (2, 0, 0), material = "CellBlue")
+cell2 = goo.Cell(name_string="Cell2_", loc=(2, 0, 0), material="CellBlue")
 goo.make_cell(cell2)
