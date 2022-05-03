0acb3133432600aaf0b903f06720d1b8dabd2563
Sean Megason <megason@hms.harvard.edu>
2022-05-02 21:14:32 -0400

    Update dividing_clump.py

--- simulations/dividing_clump.py
+++ simulations/dividing_clump.py
@@ -1,12 +1,22 @@
-# dividing_clump.py - simulates one cell growing
-# and dividing repeatedly to form a clump of cells
+# Import Libraries and setup world
+from goo import goo
+import importlib
+importlib.reload(goo)
+import bpy
+goo.setup_world()
 
-from Goo import Goo
+# Add cells and cell collections
+bpy.app.handlers.frame_change_post.clear()
+master_coll = bpy.context.view_layer.layer_collection
+collection = bpy.context.blend_data.collections.new(name='sphere')
+bpy.context.collection.children.link(collection)
+bpy.context.view_layer.active_layer_collection = bpy.context.view_layer.layer_collection.children['sphere']
+goo.make_cell(goo.Cell(name_string = "sphere(0,0,0)", loc = (0,0,0)))
 
-Goo.setup_world()
-
-cell = Goo.Cell("cell_", loc=(0, 0, 0))
-
-# make cell grow
-
-# make cell divide
+# Add handlers for division, growth and adhesion
+handlers = goo.handler_class()
+handlers.active_cell_types += ['sphere']
+handlers.set_division_rate('sphere',20)
+handlers.set_growth_rate('sphere',20)
+bpy.app.handlers.frame_change_post.append(handlers.div_handler)
+bpy.app.handlers.frame_change_post.append(handlers.growth_handler)
