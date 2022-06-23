cd41471390a402f99dd9683bb32adb4a594c112a
Ahmed Almaghasilah <ahmed.almaghasilah@maine.edu>
2022-06-23 17:42:21 -0400

    Update Doublet

--- simulations/doublet.py
+++ simulations/doublet.py
@@ -2,9 +2,160 @@
 # with different balances of cortical tension and cell adhesion
 
 from goo import goo
+import bpy
 
 goo.setup_world()
 
-# make cells
-cell1 = Goo.Cell("cell_", loc=(1, 0, 0))
-cell2 = Goo.Cell("cell_", loc=(-1, 0, 0))
+# Delete all existing collections 
+#for collection in bpy.data.collections:  # loop through the existing collection
+#    bpy.data.collections.remove(collection)
+    
+# Delete all existing collections 
+for collection in bpy.data.collections:  # loop through the existing collection
+    # loop through objects in collection
+    for objs in collection.objects:
+        # delete existing objects in collection 
+        bpy.data.objects.remove(objs)
+    # Delete collection
+    bpy.data.collections.remove(collection)
+
+#================== Cell A Collection ==================
+# Create a collection for cell A
+cA_collection = bpy.data.collections.new("A_Cells")
+# link the collection to the scene for visualization 
+bpy.context.scene.collection.children.link(cA_collection)
+# Define cell A1
+cA1 = goo.Cell("cell_A1", loc = (2,2,0))
+# Make a Blender mesh object for cell
+goo.make_cell(cA1)
+# The created cell is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene 
+# if you do not delete them the object outside colleciton will link to object inside the collection
+bpy.ops.collection.objects_remove_all()
+# Add the active cell to our specific collection 
+bpy.data.collections['A_Cells'].objects.link(obj)
+
+# Define cell A2
+cA2 = goo.Cell("cell_A2", loc = (2,-2,0))
+# Make a Blender mesh object for cell
+goo.make_cell(cA2)
+# The created cell is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active cell to our specific collection 
+bpy.data.collections['A_Cells'].objects.link(obj)
+
+#================== Cell B Collection ==================
+# Create a collection for cell B
+cB_collection = bpy.data.collections.new("B_Cells")
+# link the collection to the scene
+bpy.context.scene.collection.children.link(cB_collection)
+# Define cell A1
+cB1 = goo.Cell("cell_B1", loc = (-2,2,0))
+# Make a Blender mesh object for cell
+goo.make_cell(cB1)
+# The created cell is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene 
+# if you do not delete them the object outside colleciton will link to object inside the collection
+bpy.ops.collection.objects_remove_all()
+# Add the active cell to our specific collection 
+bpy.data.collections['B_Cells'].objects.link(obj)
+
+# Define cell B2
+cB2 = goo.Cell("cell_B2", loc = (-2,-2,0))
+# Make a Blender mesh object for cell
+goo.make_cell(cB2)
+# The created cell is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active cell to our specific collection 
+bpy.data.collections['B_Cells'].objects.link(obj)
+
+#================== Force A Collection ==================
+# Create a collection for force A
+fA_collection = bpy.data.collections.new("A_Forces")
+# link the collection to the scene 
+bpy.context.scene.collection.children.link(fA_collection)
+# Define and link force A1
+fA1 = goo.Force("force_A1", "cell_A1", -800)
+# Make force
+goo.make_force(fA1)
+# The created force is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active force to our specific collection 
+bpy.data.collections['A_Forces'].objects.link(obj)
+
+# Define and link force A2
+fA2 = goo.Force("force_A2", "cell_A2", -800)
+# Make force
+goo.make_force(fA2)
+# The created force is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active force to our specific collection 
+bpy.data.collections['A_Forces'].objects.link(obj)
+
+#================== Force B Collection ==================
+# Create a collection for force B
+fB_collection = bpy.data.collections.new("B_Forces")
+# link the collection to the scene 
+bpy.context.scene.collection.children.link(fB_collection)
+# Define and link force B1
+fB1 = goo.Force("force_B1", "cell_B1", -800)
+# Make force
+goo.make_force(fB1)
+# The created force is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active force to our specific collection 
+bpy.data.collections['B_Forces'].objects.link(obj)
+
+# Define and link force B2
+fB2 = goo.Force("force_B2", "cell_B2", -800)
+# Make force
+goo.make_force(fB2)
+# The created force is the active object
+obj = bpy.context.active_object
+# Remove object from all collections not used in a scene
+bpy.ops.collection.objects_remove_all()
+# Add the active force to our specific collection 
+bpy.data.collections['B_Forces'].objects.link(obj)
+
+
+
+
+## Cell A force
+#fA_collection = bpy.data.collections.new("A_Forces")       # create a new collection
+#bpy.context.scene.collection.children.link(fA_collection) # make collection visible on the outliner
+
+#fA1 = goo.Force("force_A1", "cell_A1", -800)
+#goo.make_force(fA1)
+
+## Cell B force
+#fB_collection = bpy.data.collections.new("B_Forces")
+#bpy.context.scene.collection.children.link(fB_collection)
+
+
+
+#cA_collection = bpy.data.collections.new
+
+#bpy.context.scene.collection.children.link(bpy.data.collections.new("A_Cell"))
+
+#cB_collection = 
+#fA_collection = 
+
+## Rename existing collection
+#coll1 = bpy.data.collections[0]
+#coll1.name
+
+## make cells
+#cell1 = Goo.Cell("cell_", loc=(1, 0, 0))
+#cell2 = Goo.Cell("cell_", loc=(-1, 0, 0))
