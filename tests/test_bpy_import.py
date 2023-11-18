1d73ab02e8c9b19ced0f02964baa003327728c9f
antoineruzette <antoine.ruzette@gmail.com>
2023-11-18 11:46:10 +0100

    Setting up tests in Blender via gh workflows

--- tests/test_bpy_import.py
+++ tests/test_bpy_import.py
@@ -1,11 +1,10 @@
 import pytest
-from testing_utils import parametrize_plugin_on_off
 
 
 @pytest.mark.parametrize(
     "imported", ("bpy"), ids=("import bpy")
 )
-@parametrize_plugin_on_off
+
 def test_bpy_import(testing_context, imported, plugin_args, expected_exitcode):
     with testing_context(
         {
