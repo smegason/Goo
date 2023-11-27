348f9780524fbd300934125989ebb23f56e9325c
antoineruzette <antoine.ruzette@gmail.com>
2023-11-27 17:35:50 +0100

    testing pytest in blender 3.3.

--- .github/load_goo.py
+++ .github/load_goo.py
@@ -1,11 +1,5 @@
 import bpy
 
 
-# Set the path to your scripts folder
-scripts_path = "/path/to/your/scripts"
-
-# Access the preferences
-prefs = bpy.context.preferences
-
-# Set the scripts path
-prefs.filepaths.script_directory = scripts_path
+print('Hello World!')
+print(bpy.context.scene)
