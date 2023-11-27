3559f1e8c827c96530c91f545a952822c0c089fa
antoineruzette <antoine.ruzette@gmail.com>
2023-11-27 18:44:57 +0100

    testing pytest in blender 3.3.

--- .github/load_goo.py
+++ .github/load_goo.py
@@ -1,14 +1,15 @@
-import bpy
-print('bpy imported')
+try:
+    import bpy
+except ImportError:
+    print("Error: Unable to import 'bpy'")
 
-# Set the path to your scripts folder
-scripts_path = "/home/runner/work/Goo/Goo/scripts/"
-print(scripts_path)
+try:
+    import sys
+except ImportError:
+    print("Error: Unable to import 'sys'")
 
-# Access the preferences
-prefs = bpy.context.preferences
-
-# Set the scripts path
-prefs.filepaths.script_directory = scripts_path
-
-from goo import goo
+try:
+    sys.path.append("/home/runner/work/Goo/Goo/scripts/modules/goo/")
+    from goo import goo
+except ImportError:
+    print("Error: Unable to import 'goo'")
