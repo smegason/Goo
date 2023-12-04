78602ac6a2a7f3ea6077c272a33fbbf2c94a0fda
antoineruzette <antoine.ruzette@gmail.com>
2023-12-04 18:34:50 -0500

    Automatically enable addons in goo #34

--- .github/check_hdr_file.py
+++ .github/check_hdr_file.py
@@ -1,7 +1,6 @@
 import os
 
-file_path = '/home/runner/blender/3.0/scripts/modules/\
-    goo/missile_launch_facility_01_4k.hdr'
+file_path = '/home/runner/blender/3.0/scripts/modules/goo/missile_launch_facility_01_4k.hdr'
 
 if os.path.exists(file_path):
     print("File exists")
