01191e340e8b14478ed9ffe038c9c614590a36ba
antoineruzette <antoine.ruzette@gmail.com>
2024-04-24 09:55:12 -0400

    Clean up simulations folder

--- simulations/cell_division_with_adhesion.py
+++ simulations/cell_division_with_adhesion.py
@@ -3,14 +3,11 @@ from importlib import reload
 reload(goo)
 goo.setup_world()
 
-  
 # Cells A 
-# Define cell A1
 goo.make_cell("cell_A1", loc=(-1.9, 0, 0), type="A_Cells", scale=(1.67, 1.67, 1.67))
 goo.make_cell("cell_A2", loc=(1.9, 0, 0), type="A_Cells", scale=(1.67, 1.67, 1.67))
 
 # Forces A
-
 homoA = 7500
 
 # Define force A1
@@ -20,8 +17,8 @@ goo.add_homo_adhesion('cell_A2', -homoA)
 # Simulation setup 
 handlers = goo.handler_class()
 handlers.launch_simulation(start=1,  # default, 1
-                           end=120,  # default, 250
-                           filepath="/Users/antoine/Harvard/MegasonLab/GPU_backup/AntoineRuzette/goo/data/division/20240311_division_with_adhesion/20240311_division_with_adhesion", 
+                           end=110,  # default, 250
+                           filepath="", 
                            adhesion=True,  # default, True
                            data=True,  # default, False
                            growth=True, 
