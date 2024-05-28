7ae3c2a420b853582f1d6385151bde8cfec16ee7
antoineruzette <antoine.ruzette@gmail.com>
2024-05-28 10:33:06 -0400

    Update package info

--- scripts/modules/goo/__init__.py
+++ scripts/modules/goo/__init__.py
@@ -16,3 +16,7 @@ sys.path.append(
 sys.path.append(
     "/Users/antoine/.local/lib/python3.10/site-packages"
 )
+
+__version__ = "1.0.0"
+__author__ = 'Antoine A. Ruzette, Charles Dai, Sean Megason'
+__credits__ = 'Harvard Medical School'
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='goo',
-    version='0.0.1',    
-    description='Goo is a Python library to simulate biological cells in 3D\
-          natively in Blender.',
+    version='1.0.0',    
+    description='Goo is a library to simulate 3D biological cells, tissues and\
+          embryos in Blender.',
     url='https://github.com/smegason/goo',
     author='Antoine A. Ruzette, Sean Megason',
     author_email='sean_megason@hms.harvard.edu',
@@ -22,7 +22,10 @@ setup(
         'License :: OSI Approved :: BSD License',  
         'Operating System :: POSIX :: Linux',        
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
