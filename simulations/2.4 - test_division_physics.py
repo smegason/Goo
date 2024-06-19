9810382b9433f498d3a2d0f3fd2527dbdcda0d5c
antoineruzette <antoine.ruzette@gmail.com>
2024-06-19 10:23:22 -0400

    Switch branch

--- simulations/2.4 - test_division_physics.py
+++ simulations/2.4 - test_division_physics.py
@@ -1,6 +1,7 @@
 from importlib import reload
 import goo
-from goo import * 
+from goo.division import *
+from goo.handler import *
 
 reload(goo)
 goo.reset_modules()
@@ -8,16 +9,22 @@ goo.reset_scene()
 
 celltype = goo.SimpleType("cellA")
 celltype.homo_adhesion_strength = 5000
-cell = celltype.create_cell("cell", (0, 0, 0))
-cell.stiffness = 15
+cell = celltype.create_cell(name="cell", loc=(0, 0, 0), color=(0, 1, 1))
+cell.stiffness = 5
 cell.pressure = 5
 
 sim = goo.Simulator(celltypes=[celltype])
 sim.setup_world()
 sim.add_handlers(
+    
     [
-        TimeDivisionHandler(BisectDivisionLogic, mu=20),
+        GrowthPIDHandler(target_volume=75),
+        SizeDivisionHandler(BisectDivisionLogic, mu=60, sigma=10),  # TimeDivisionHandler
         AdhesionLocationHandler(),
         RemeshHandler(),
+        RandomMotionHandler(distribution=ForceDist.CONSTANT, max_strength=0),
     ]
 )
+
+# Run simulation headless
+# sim.run(65)
