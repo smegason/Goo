4c47baa865f689a049e1398fec6ac878568b7601
Charles Dai <charles.r.dai@gmail.com>
2024-07-02 20:33:44 -0400

    Clean up repo

--- scripts/modules/goo/__init__.py
+++ scripts/modules/goo/__init__.py
@@ -1,9 +1,3 @@
-import sys
-
-sys.path.append(
-    "/Users/charlesdai/dev/goo_project/Goo/.venv/lib/python3.10/site-packages"
-)
-
 from .cell import create_cell, CellType, YolkType, SimpleType
 from .reloader import *
 from .simulator import Simulator
